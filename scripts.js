ce3fe5d7d411db0b5181a904fa8d90d4300f9328
Raja24-k <129048263+Raja24-k@users.noreply.github.com>
2024-12-06 19:37:05 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function handleAction(action) {
+  if (action === "customize") {
+    alert("Start customizing your products today!");
+  } else if (action === "startSelling") {
+    alert("Learn how to start selling kids wear!");
+  } else if (action === "about") {
+    alert("Discover more about Printlet's amazing services.");
+  }
+}
+  
